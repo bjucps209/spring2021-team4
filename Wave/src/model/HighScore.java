16cf9d1c6edda52bea4db0dba1232757c6a0edf3
Nicholas-Rich <70034588+Nicholas-Rich@users.noreply.github.com>
2021-04-03 11:28:59 -0400

    Highscores added

--- Wave/src/model/HighScore.java
+++ Wave/src/model/HighScore.java
@@ -1,5 +1,44 @@
 package model;
 
-public class HighScore {
-    // Object containing player name and score
+import java.util.ArrayList;
+
+public class HighScore implements Comparable<User> {
+    // List containing player name and score
+
+    private ArrayList<User> list = new ArrayList<User>();
+
+    public HighScore(ArrayList<User> list) {
+        this.list = list;
+    }
+
+    // will modify the Collections.sort method to sort by score
+    @Override
+    public int compareTo(User o) {
+        return 0;
+    }
+
+    // Use collections to properly sort the list
+    public ArrayList<User> sort() {
+        return null;
+    }
+
+    public ArrayList<User> getList() {
+        return list;
+    }
+    
+    public void setList(ArrayList<User> list) {
+        this.list = list;
+    }
+
+    // Save the name and score to a file 
+    public void save() {
+
+    }
+
+    // Update the list with information from the save file
+    public void load() {
+
+    }
+
+
 }
