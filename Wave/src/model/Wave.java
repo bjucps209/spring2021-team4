3e6d4ea1eccd152bf13ae5ff91d74e987951c308
joeldushouyu <joeldushouyu@gmail.com>
2021-04-05 23:12:03 -0400

    update user profile

--- Wave/src/model/Wave.java
+++ Wave/src/model/Wave.java
@@ -1,7 +1,7 @@
 package model;
 
 import java.util.ArrayList;
-
+import java.io.*;
 import model.Enums.*;
 import model.GameObjects.EnemyObject;
 import model.GameObjects.GameObject;
@@ -18,7 +18,7 @@ public class Wave {
     private final int windowHeight = 0;
 
     // Data variables
-    private ArrayList<User> users = new ArrayList<User>();  // A list of exist user
+    private ArrayList<User> users = new ArrayList<User>(); // A list of exist user
     private ArrayList<HighScore> highScores = new ArrayList<HighScore>();
     private ArrayList<ShipSkins> skins = new ArrayList<ShipSkins>();
 
@@ -34,7 +34,7 @@ public class Wave {
 
     // Singleton constructor
     private Wave() {
-        
+
     }
 
     public void loadUser() {
@@ -46,11 +46,6 @@ public class Wave {
         game = new Game(500, 500);
     }
 
-    // Saves user
-    public void saveUser() {
-
-    }
-
     // Singleton get instance method
     public static Wave getInstance() {
         if (wave == null) {
@@ -60,7 +55,7 @@ public class Wave {
         return wave;
     }
 
-    public Game getGame(){
+    public Game getGame() {
         return this.game;
     }
 
@@ -72,45 +67,127 @@ public class Wave {
         return this.windowHeight;
     }
 
-    public ArrayList<User> getUsers() {
-        return this.users;
+    /**
+     * This function take a String userName, check and return User() in this.users that has the same user name
+     * @param userName - String indicate user name
+     * @return - user() that has the same name as userName. Or return null for does not exist
+     */
+    public User userExist(String userName) {
+        // assuming that no two user will holds the same name
+        // case sentative
+        for(User existUser : this.users){
+            if(existUser.getName().equals(userName)){
+                return existUser;
+            }
+        }
+        // means did not find
+        return null;
     }
 
-    public void setUsers(ArrayList<User> users) {
-        this.users = users;
+    // Saves user
+    /**
+     * The function will add this.currentUser into this.users if does not exist in the list.
+     * If exist, will replace that User() to this.user's reference
+     * Condition for exist: one of User() in this.users has the same Name as this.currentUser
+     */
+    public void saveCurrentUser() {
+        // Simply replace this.user with the same user name in this.users
+        User existUser = userExist(currentUser.getName());
+        if(existUser != null){
+            // now just change the reference
+            existUser = this.currentUser;
+        }else{
+            // means this is a new user that does not exist in the ArrayList
+            this.users.add(this.currentUser);
+        }
+    }
+
+    public boolean saveAllUsers() {
+        try(PrintWriter wd = new PrintWriter(new FileWriter("userInfo.txt"))){
+
+            wd.println(this.users.size());
+            for(User existUser : this.users){
+                wd.println(existUser.serialization());
+            }
+            return true;
+        }catch (IOException e){
+            return false;
+        }
+    }
+
+    public boolean loadAllUsers() {
+        try (BufferedReader rd = new BufferedReader(new FileReader("userInfo.txt"))) {
+            int numberUser = Integer.parseInt(rd.readLine());
+            users = new ArrayList<User>();
+            for (int i = 0; i < numberUser; i++) {
+
+                String[] information = rd.readLine().split(";");
+                User currentUser = new User();
+
+                if (information.length != 4) {
+                    // means an error in the information
+                    throw new IOException("error in converting file");
+                }
+                
+                else if(currentUser.deserialization(information) == false){
+                    // means have error in data
+                    throw new IOException("error in converting file");
+                }
+                else{
+                    users.add(currentUser);
+                }
+
+            }
+            return true;
+        } catch (IOException e) {
+            // means failed in loading the file
+            return false;
+        } catch (Exception e) {
+            // means error in file
+            return false;
+        }
+    }
+
+    // --- getters ---
+    public ArrayList<User> getUsers() {
+        return this.users;
     }
 
     public ArrayList<HighScore> getHighScores() {
         return this.highScores;
     }
 
-    public void setHighScores(ArrayList<HighScore> highScores) {
-        this.highScores = highScores;
-    }
-
     public ArrayList<ShipSkins> getSkins() {
         return this.skins;
-    }
-
-    public void setSkins(ArrayList<ShipSkins> skins) {
-        this.skins = skins;
     }
 
     public ShipSkins getCurrentShip() {
         return this.currentShip;
     }
 
-    public void setCurrentShip(ShipSkins currentShip) {
-        this.currentShip = currentShip;
-    }
-
     public User getCurrentUser() {
         return currentUser;
+    }
+    // --- setters ---
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public void setHighScores(ArrayList<HighScore> highScores) {
+        this.highScores = highScores;
+    }
+
+    public void setSkins(ArrayList<ShipSkins> skins) {
+        this.skins = skins;
+    }
+
+    public void setCurrentShip(ShipSkins currentShip) {
+        this.currentShip = currentShip;
     }
 
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
     }
 
-    
 }
