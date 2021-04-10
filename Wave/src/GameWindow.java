a91cd3a05c92c8f88a9aef85611c17765d84c435
connerchrist <cchri679@students.bju.edu>
2021-04-10 17:36:53 -0400

    updated gamewindow.java

--- Wave/src/GameWindow.java
+++ Wave/src/GameWindow.java
@@ -1,3 +1,60 @@
+import javafx.fxml.FXML;
+import model.Game;
+import model.Wave;
+import model.GameObjects.EnemyObject;
+import model.GameObjects.Player;
+
 public class GameWindow {
-    
+
+    Wave w;
+    Player p;
+    Game g;
+
+    @FXML
+    public void initialize() {
+        w = Wave.getInstance();
+        w.gameStart();
+        g = w.getGame();
+        spawnEntities();
+    }
+
+    public void spawnEntities() {
+        // code to combine all spawn function below
+        spawnPlayer();
+        spawnEnemies();
+        spawnObstacles();
+    }
+
+    public void spawnPlayer() {
+        // Code to spawn gui player
+    }
+
+    public void spawnEnemies() {
+        for (EnemyObject o : g.getLevels().get(g.getLevelNum()).getEnemies()) {
+            switch (o.getType()) {
+            case BOUNCER:
+
+                break;
+            case GHOST:
+
+                break;
+            case LASER:
+
+                break;
+            case SHAPESHIFTER:
+
+                break;
+            case TRACKER:
+
+                break;
+            default:
+
+                break;
+            }
+        }
+    }
+
+    public void spawnObstacles() {
+        // code to spawn obstacles
+    }
 }
