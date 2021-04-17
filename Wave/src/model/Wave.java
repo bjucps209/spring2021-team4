f111cd149f3349f44b152b77c27c6f17fb734fb3
Ryan Raddatz <rradd468@students.bju.edu>
2021-04-17 16:11:34 -0400

    fixed testlevel

--- Wave/src/model/Wave.java
+++ Wave/src/model/Wave.java
@@ -303,7 +303,7 @@ public class Wave {
 
             for (String instance : instances) {
                 String[] instanceInfo = instance.split(Pattern.quote(","));
-
+                System.out.println(instanceInfo.length);
                 GameObject object;
                 // enemy entities
                 if (instanceInfo[0].equals("Bouncer")) {
