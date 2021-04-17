98d0973d3bf0fc75b147c52bc0ea956d5c91c49e
connerchrist <cchri679@students.bju.edu>
2021-04-17 15:06:35 -0400

    update

--- Wave/src/model/GameObjects/Enemies/Bouncer.java
+++ Wave/src/model/GameObjects/Enemies/Bouncer.java
@@ -5,6 +5,8 @@ import model.Level;
 public class Bouncer extends EnemyObject {
     public Bouncer(Level l) {
         super(l);
+        setWidth(50);
+        setHeight(50);
     }
 
     public static int speed = 5;
